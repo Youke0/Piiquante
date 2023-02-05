7b5adb68749e977f9591fb886123847fab4bcda6
Youke0 <108680055+Youke0@users.noreply.github.com>
2023-02-05 19:16:36 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,3 @@
-//Création de serveur
-/*
-const http = require('http')
-const app = require('./app')
-
-app.set('port', process.env.PORT || 3000)
-const server = http.createServer(app)
-
-server.listen(process.env.PORT || 3000)
-*/
-
 const http = require("http")
 const app = require("./app")
 
