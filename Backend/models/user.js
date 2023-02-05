5bce951240f3a37af46fda10417bb3226df71e98
Youke0 <108680055+Youke0@users.noreply.github.com>
2023-02-05 19:15:47 +0100

    Update user.js

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
-
-//Permet d'avoir un email unique pour chaque utilisateur
 const uniqueValidator = require('mongoose-unique-validator')
 
-//Schéma de création d'utilisateur avec une adresse mail unique
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
